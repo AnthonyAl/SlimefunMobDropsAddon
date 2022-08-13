9ff3c07a5676b0c013b3a139e2a0cd55b8ee003c
AnthonyAl <p19147@unipi.gr>
2022-08-13 19:11:38 +0300

    Basic logic implementation.

--- src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Core/Utils.java
+++ src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Core/Utils.java
@@ -1,7 +1,12 @@
 package com.unipi.alexandris.slimefunaddon.slimefunmobdrops.Core;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Utils {
 
-
+    public static boolean calcDropChance(double rarity) {
+        ThreadLocalRandom r = ThreadLocalRandom.current();
+        return r.nextDouble(100) < rarity;
+    }
 
 }
--- src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Handlers/EventsHandler.java
+++ src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Handlers/EventsHandler.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import java.util.List;
+import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -23,11 +24,19 @@ public final class EventsHandler implements Listener {
 
     @EventHandler
     public void onVanillaPerish(EntityDeathEvent event) {
+        if(!plugin.config.isVanilla_enabled()) return;
+        if(plugin.config.isMythic_enabled())
+            if(((MythicBukkit) (Objects.requireNonNull(getServer().getPluginManager().getPlugin("MythicMobs")))).getAPIHelper().isMythicMob(event.getEntity())) return;
+
+        List<String> commons = plugin.config.get_items(event.getEntityType(), 0);
+        List<String> rares = plugin.config.get_items(event.getEntityType(), 1);
+
 
     }
 
     @EventHandler
     public void onMythicPerish(MythicMobDeathEvent event) {
+        if(!plugin.config.isMythic_enabled()) return;
 
     }
 }
