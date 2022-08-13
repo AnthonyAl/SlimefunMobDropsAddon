316d483405653a14d0d4b11074e0ce29c7ffeb02
AnthonyAl <p19147@unipi.gr>
2022-08-13 19:43:11 +0300

    Item Drop Logic implementation.

--- src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Handlers/EventsHandler.java
+++ src/main/java/com/unipi/alexandris/slimefunaddon/slimefunmobdrops/Handlers/EventsHandler.java
@@ -1,17 +1,22 @@
 package com.unipi.alexandris.slimefunaddon.slimefunmobdrops.Handlers;
 
 
+import com.unipi.alexandris.slimefunaddon.slimefunmobdrops.Core.Utils;
 import com.unipi.alexandris.slimefunaddon.slimefunmobdrops.SlimefunMobDrops;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemSpawnReason;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.bukkit.events.MythicMobDeathEvent;
-import org.bukkit.Location;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 import java.util.Objects;
 
+import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getServer;
 
 public final class EventsHandler implements Listener {
@@ -31,12 +36,66 @@ public final class EventsHandler implements Listener {
         List<String> commons = plugin.config.get_items(event.getEntityType(), 0);
         List<String> rares = plugin.config.get_items(event.getEntityType(), 1);
 
+        //For the common items (all of them can drop at one time)
+        for(String data : commons) {
+            String slimefun_item = data.split(" ")[0];
+            double drop_rate = Double.parseDouble(data.split(" ")[1]);
 
+            ItemStack itemStack = Objects.requireNonNull(SlimefunItem.getById(slimefun_item)).getItem();
+
+            if(Utils.calcDropChance(drop_rate)) {
+                getLogger().info("Spawned slimefun item " + slimefun_item + " as a result of a Vanilla Common Mob Drop");
+                SlimefunUtils.spawnItem(event.getEntity().getLocation(), itemStack, ItemSpawnReason.MISC);
+            }
+        }
+
+        //For the rare items (only one can drop at a time)
+        for(String data : rares) {
+            String slimefun_item = data.split(" ")[0];
+            double drop_rate = Double.parseDouble(data.split(" ")[1]);
+
+            ItemStack itemStack = Objects.requireNonNull(SlimefunItem.getById(slimefun_item)).getItem();
+
+            if(Utils.calcDropChance(drop_rate)) {
+                getLogger().info("Spawned slimefun item " + slimefun_item + " as a result of a Vanilla Rare Mob Drop");
+                SlimefunUtils.spawnItem(event.getEntity().getLocation(), itemStack, ItemSpawnReason.MISC);
+                break;
+            }
+        }
     }
 
     @EventHandler
     public void onMythicPerish(MythicMobDeathEvent event) {
         if(!plugin.config.isMythic_enabled()) return;
 
+        List<String> commons = plugin.config.get_items(event.getEventName(), 0);
+        List<String> rares = plugin.config.get_items(event.getEventName(), 1);
+
+        //For the common items (all of them can drop at one time)
+        for(String data : commons) {
+            String slimefun_item = data.split(" ")[0];
+            double drop_rate = Double.parseDouble(data.split(" ")[1]);
+
+            ItemStack itemStack = Objects.requireNonNull(SlimefunItem.getById(slimefun_item)).getItem();
+
+            if(Utils.calcDropChance(drop_rate)) {
+                getLogger().info("Spawned slimefun item " + slimefun_item + " as a result of a Mythic Mob Common Mob Drop");
+                SlimefunUtils.spawnItem(event.getEntity().getLocation(), itemStack, ItemSpawnReason.MISC);
+            }
+        }
+
+        //For the rare items (only one can drop at a time)
+        for(String data : rares) {
+            String slimefun_item = data.split(" ")[0];
+            double drop_rate = Double.parseDouble(data.split(" ")[1]);
+
+            ItemStack itemStack = Objects.requireNonNull(SlimefunItem.getById(slimefun_item)).getItem();
+
+            if(Utils.calcDropChance(drop_rate)) {
+                getLogger().info("Spawned slimefun item " + slimefun_item + " as a result of a Mythic Mob Rare Mob Drop");
+                SlimefunUtils.spawnItem(event.getEntity().getLocation(), itemStack, ItemSpawnReason.MISC);
+                break;
+            }
+        }
     }
 }
